79a8f07294e9ce253c979952a432187ae3638a1f
Mark <42696773+MarkHmnv@users.noreply.github.com>
2023-07-27 14:35:55 +0300

    Implement Generation of ID and Role Headers  (#5)
    * Remove unnecessary property from application.properties
    
    * Add logging for internal server errors in GlobalExceptionHandler
    
    * Refactor JwtService for token validation and user info extraction
    
    * Update JwtConstants with User ID and Role
    
    * Refactor Jwt Validation Filter to add user info in headers
    
    * Remove redundant gateway application tests

--- src/main/java/com/uptalent/gateway/jwt/JwtConstants.java
+++ src/main/java/com/uptalent/gateway/jwt/JwtConstants.java
@@ -1,5 +1,9 @@
 package com.uptalent.gateway.jwt;
 
-public class JwtConstants {
+public final class JwtConstants {
+    private JwtConstants(){}
+
     public static final String BEARER_PREFIX = "Bearer ";
+    public static final String USER_ID_KEY = "User-Id";
+    public static final String USER_ROLE_KEY = "User-Role";
 }
