c2bfeb30dd7011e08496eb650b20f10bf0bd7360
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-30 17:30:48 +0300

    Remove @EnableScheduling from API Gateway

--- src/main/java/io/github/uptalent/ApiGatewayApplication.java
+++ src/main/java/io/github/uptalent/ApiGatewayApplication.java
@@ -3,11 +3,9 @@ package io.github.uptalent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableScheduling
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
